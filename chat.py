1a822bc1f5d9f1672928f70791eaaf6f6f48c993
makora9143 <makoto.kawano@gmail.com>
2015-05-13 17:48:36 +0900

    過去ログを参照可能にした

--- chat.py
+++ chat.py
@@ -4,12 +4,16 @@
 import os
 import json
 import datetime
+import redis
+import pickle
 from tornado import httpserver, web, websocket, ioloop, options
+
+
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 
 class ChatWebSocket(websocket.WebSocketHandler):
     """Class for Web Socket"""
-    # websocket connections manager
     connections = set()
 
     def check_origin(self, origin):
@@ -17,24 +21,33 @@ class ChatWebSocket(websocket.WebSocketHandler):
 
     def open(self):
         """Call when the websocket opens"""
-        # append self connection to manager
         ChatWebSocket.connections.add(self)
+        self.get_past_message()
         print "Websocket opened"
 
     def on_message(self, message):
         jmsg = json.loads(message)
         d = datetime.datetime.today()
         jmsg[u'time'] = u'%s:%s:%s' % (d.hour, d.minute, d.second)
+        self.check_message(jmsg)
         for connection in ChatWebSocket.connections:
             try:
-                # connection.write_message(message)
                 connection.write_message(jmsg)
             except:
                 self.connections.remove(connection)
 
     def on_close(self):
-        # delete self connection to manager
         ChatWebSocket.connections.remove(self)
+
+    def check_message(self, message):
+        if message['type'] == 'chat':
+            if r.llen('msg') > 20:
+                r.lpop('msg')
+            r.rpush('msg', pickle.dumps(message))
+
+    def get_past_message(self):
+        for message in r.lrange('msg', 0, 20):
+            self.write_message(pickle.loads(message))
 
 
 class MainHandler(web.RequestHandler):
@@ -46,7 +59,6 @@ class MainHandler(web.RequestHandler):
 class Application(web.Application):
     """Override web.Application class"""
     def __init__(self):
-        # regist url routes
         handlers = [
                     (r'/', MainHandler),
                     (r'/websocket', ChatWebSocket),
